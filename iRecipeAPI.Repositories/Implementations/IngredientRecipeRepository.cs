c9576e18716ad2de4284f7c7ddb065b0dbe83c55
João Clarinha <joaoclarinha13@gmail.com>
2024-10-07 16:48:47 +0100

    minor changes

---
+++ iRecipeAPI.Repositories/Implementations/IngredientRecipeRepository.cs
@@ -0,0 +1,43 @@
+﻿using iRecipeAPI.Data.Context;
+using iRecipeAPI.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using iRecipe.Domain;
+
+namespace iRecipeAPI.Repositories.Implementations
+{
+    public  class IngredientRecipeRepository : IIngredientRecipeRepositoy
+    {
+        private readonly DbSet<IngredientRecipe> _dbSet;
+        private readonly iRecipeAPIDBContext _irecipeAPIDBContext;
+
+        public IngredientRecipeRepository(iRecipeAPIDBContext irecipeAPIDBContext)
+        {
+            _dbSet = irecipeAPIDBContext.Set<IngredientRecipe>();
+            _irecipeAPIDBContext = irecipeAPIDBContext;
+        }
+
+        public List<IngredientRecipe> GetAll()
+        {
+            return _dbSet.ToList();
+        }
+
+        public List<IngredientRecipe> GetAllByRecipeId(int recipeId)
+        {
+            return _dbSet.Where(p => p.RecipeId == recipeId).Include(p => p.Ingredient).ToList();
+        }
+
+        public IngredientRecipe GetById(int id)
+        {
+            return _dbSet
+                .Where(p => p.Id == id)
+                .Include(p => p.Recipe)
+                .ThenInclude(p => p.User)
+                .Include(p => p.Ingredient)
+                .ThenInclude(p => p.Unit)
+                .FirstOrDefault();
+        }
+
+
+
+    }
+}
